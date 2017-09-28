8c1f48d8f351ed03e11ab0bb7dc4a03a916be83a
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-09-28 18:30:19 -0400

    extract printing logic out of the actual traversal

--- go/src/github.com/karlhungus/tree_printer/tree_printer.go
+++ go/src/github.com/karlhungus/tree_printer/tree_printer.go
@@ -3,6 +3,8 @@ package main
 //import "os"
 import "fmt"
 
+type NodeExecuter func(node Node, depth int)
+
 func main() {
 	tree := Tree{&Node{Value: 5, Left: &Node{Value: 4}, Right: &Node{Value: 6}}}
 	output := printTree(&tree)
@@ -10,36 +12,50 @@ func main() {
 }
 
 func printTree(tree *Tree) string {
-	return breadthTraversal(tree)
+	printArray := make([][]int, 0)
+	breadthTraversal(tree, func(node Node, depth int) {
+		if len(printArray) <= depth {
+			printArray = append(printArray, make([]int, 0))
+		}
+		printArray[depth] = append(printArray[depth], node.Value)
+	})
+
+	output := ""
+	for _, array := range printArray {
+		for i, value := range array {
+			if i >= 0 && i < len(array)-1 {
+				output += fmt.Sprintf("%v,", value)
+			} else {
+				output += fmt.Sprintf("%v", value)
+			}
+		}
+		output += fmt.Sprintf("\n")
+	}
+	return output
 }
 
-func breadthTraversal(tree *Tree) string {
+func breadthTraversal(tree *Tree, executor NodeExecuter) string {
 	output := ""
 	queue := make([]*Node, 0)
-	set := make(map[*Node]bool)
+	set := make(map[*Node]int)
 
-	set[tree.Root] = true
+	set[tree.Root] = 0
 	queue = append(queue, tree.Root)
 
 	for len(queue) > 0 {
 		current := queue[0]
 		queue = queue[1:]
-		if output != "" {
-			output += fmt.Sprintf(",%d", current.Value)
-		} else {
-			output += fmt.Sprintf("%d", current.Value)
-		}
-		fmt.Printf("length: %d, value: %v", len(queue), current.Value)
+		executor(*current, set[current])
 
 		if current.Left != nil {
 			if _, present := set[current.Left]; !present {
-				set[current.Left] = true
+				set[current.Left] = set[current] + 1
 				queue = append(queue, current.Left)
 			}
 		}
 		if current.Right != nil {
 			if _, present := set[current.Right]; !present {
-				set[current.Right] = true
+				set[current.Right] = set[current] + 1
 				queue = append(queue, current.Right)
 			}
 		}
