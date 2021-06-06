5d6c2e25b2442e8ee44deb430c7c8fdf496b31b9
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2021-06-06 10:43:50 -0400

    improved is anagram

--- go/src/github.com/karlhungus/leetcode/is_anagram.go
+++ go/src/github.com/karlhungus/leetcode/is_anagram.go
@@ -25,3 +25,20 @@ func isAnagram(s string, t string) bool {
 func remove(s []rune, i int) []rune {
 	return append(s[0:i], s[i+1:]...)
 }
+
+func isAnagramImproved(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	counter := make(map[byte]int)
+	for i, _ := range s {
+		counter[s[i]]++
+		counter[t[i]]--
+	}
+	for _, c := range counter {
+		if c != 0 {
+			return false
+		}
+	}
+	return true
+}
