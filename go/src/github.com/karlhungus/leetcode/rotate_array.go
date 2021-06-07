bd4ffa6c0e29fdd58d0931c3dffb809e8115a1d1
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2021-06-07 19:48:37 -0400

    improved rotate

--- go/src/github.com/karlhungus/leetcode/rotate_array.go
+++ go/src/github.com/karlhungus/leetcode/rotate_array.go
@@ -17,3 +17,27 @@ func rotate(nums []int, k int) {
 		}
 	}
 }
+
+func rotate2(nums []int, k int) {
+	visited := 0
+	// visit each position
+	for i := 0; visited < len(nums); i++ {
+		//store the first move
+		prev := nums[i]
+		// find where it should move
+		j := (i + k) % len(nums)
+		// while we haven't moved the one we are on continue
+		for i != j {
+			//do the swap, storing what was moved to
+			nums[j], prev = prev, nums[j]
+			//update what we've swapped
+			visited++
+			//calculate the next position
+			j = (j + k) % len(nums)
+		}
+		// move the last umoved item back to i
+		nums[i] = prev
+		// update visited count
+		visited++
+	}
+}
